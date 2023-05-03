1091dd7ab5ca0ae34fac9350d8779b9bab5a773d
Ayush Tiwari <46418123+Rahul-71@users.noreply.github.com>
2023-05-04 00:26:56 +0530

    pagination added over searchResults

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -21,7 +21,10 @@ const BodyPart = ({ item, bodyPart, setBodyPart }) => {
       }}
       onClick={() => {
         setBodyPart(item);
-        window.scrollTo({ top: 1800, left: 100, behavior: "smooth" });
+        // window.scrollTo({ top: 1800, left: 100, behavior: "smooth" });
+        document
+          .getElementById("searchResult")
+          .scrollIntoView({ behavior: "smooth" });
       }}
     >
       <img src={Icon} alt="dumbell" style={{ width: "60px", height: "60px" }} />
--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,12 +1,34 @@
-import { Box, Stack, Typography } from "@mui/material";
-import React from "react";
+import { Box, Pagination, Stack, Typography } from "@mui/material";
+import React, { useState } from "react";
 import ExerciseCard from "./ExerciseCard";
 
 const Exercises = ({ setExercises, exercises, setBodyPart }) => {
-  console.log(exercises);
+  // console.log(exercises);
+  const [currentPage, setcurrentPage] = useState(1);
+  const exercisePerPage = 9;
+
+  const indexOfLastExercise = currentPage * exercisePerPage;
+  const indexOfFirstExercise = indexOfLastExercise - exercisePerPage;
+  const currentPageExercises = exercises.slice(
+    indexOfFirstExercise,
+    indexOfLastExercise
+  );
+
+  const paginate = (e, value) => {
+    setcurrentPage(value);
+    document
+      .getElementById("searchResult")
+      .scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <Box>
-      <Typography variant="h3" mb="40px">
+      <Typography
+        id="searchResult"
+        sx={{ fontSize: { lg: "30px", xs: "20px" } }}
+        mb="40px"
+        fontWeight="bold"
+      >
         Showing Results
       </Typography>
 
@@ -16,9 +38,23 @@ const Exercises = ({ setExercises, exercises, setBodyPart }) => {
         justifyContent="center"
         sx={{ gap: { lg: "110px", xs: "50px" } }}
       >
-        {exercises.map((exercise, index) => (
+        {currentPageExercises.map((exercise, index) => (
           <ExerciseCard key={index} exercise={exercise} />
         ))}
+      </Stack>
+
+      {/* to add the pagination feature to explore the search result items */}
+      <Stack mt="2rem" alignItems="center">
+        {exercises.length > exercisePerPage && (
+          <Pagination
+            color="standard"
+            shape="rounded"
+            count={Math.ceil(exercises.length / exercisePerPage)}
+            size="large"
+            page={currentPage}
+            onChange={paginate}
+          />
+        )}
       </Stack>
     </Box>
   );
